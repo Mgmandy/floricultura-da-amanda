d9e5244d98c39ceb13400624eff3eb04a3623073
Mgmandy <136463339+Mgmandy@users.noreply.github.com>
2024-04-25 12:25:21 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,25 @@
+<div id="cesta-de-flores-modal" class="cesta-de-flores-modal modal-event-minicart">
+<div class="cesta-de-flores-folga">
+<div class="cesta-de-flores-modal-pelicula"></div>
+<span class="cesta-de-flores-anticlick"></span>
+<span class="cesta-de-flores-seta-cima oculta-mobile-header"></span>
+<span class="cesta-de-flores-seta-cima-mask oculta-mobile-header"></span>
+<div id="cesta-de-flores-modal-overflow" class="modal-overflow-x">
+<div class="cesta-de-flores-modal-title">
+<div class="cesta-de-flores-modal-title-icon">
+<span class="icon-icones-cesta"></span>
+</div>
+<div class="cesta-de-flores-modal-title-text">ITENS DA CESTA DE FLORES</div>
+</div>
+<a href="javascript:void(0);" title="Sair" onclick="getCestaFloresModal(0, 0);">
+<div class="xclose">
+<span class="icon-icones-fechar"></span>
+</div>
+</a>
+<div id="cesta-de-flores-modal-content" class="cesta-de-flores-modal-content"><div class="cesta-de-flores-modal-content-empty">
+										<img src="/skin/frontend/apptha/superstore/images/IlustracaoIcones/ilustracao-cesta-vazia.png" data-src="/skin/frontend/apptha/superstore/images/IlustracaoIcones/ilustracao-cesta-vazia.png">
+										<p>SUA CESTA ESTÁ VAZIA</p>
+									</div></div>
+</div>
+</div>
+</div>
