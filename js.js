490d518730c56642882ae03e648a49b1377ef5ca
Mgmandy <136463339+Mgmandy@users.noreply.github.com>
2024-05-10 09:04:54 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,15 @@
-
 let url="dados.xml";
 var id = 0;
 
 $.ajax(url)
     .done(function(xml){
-        $(xml).find("flor").each(function(){
-            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="flores">flores</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">disponivel ou esgotado</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
+        $(xml).find("pirata").each(function(){
+            $("#cards").append('<div class="card"><a href="individual.html?id='+$(this).find("id").text()+'"><p class="flores">Flores</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">disponivel ou esgotado</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+'</a> </p><\div>');
         });
     })
     .fail(function(){
         alert("Ocorreu um erro na leitura do arquivo XML.");
     })
-
-   
-
 
     var url_string = window.location.href;
     var url1 = new URL(url_string);
@@ -21,10 +17,10 @@ $.ajax(url)
 
     $.ajax(url)
     .done(function(xml){
-            $(xml).find("flor").each(function(){
+            $(xml).find("pirata").each(function(){
                 var pos = parseInt($(this).find("id").text())
                 if(id == pos){
-                $("#individual").append('<div class="card"><p class="flores">Procurado</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">disonivel ou esgotado</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
+                $("#individual").append('<div class="card"><p class="florea">Floreso</p> <img class="foto"src="'+ $(this).find("id").text() +'.jpg"><p class="vivo">disponivel ou esgotado</p> <p class="nome">'+ $(this).find("nome").text() +'</p> <p class="rec"> <img class="berrys" src="Berrys.png">'+ $(this).find("recompensa").text()+' </p><p> Status:'+ $(this).find("status").text()+' </p><\div>');
                 }
             });
         
